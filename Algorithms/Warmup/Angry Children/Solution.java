9251adbdcb4501fb8c95db89cf607e38ece0cb3d
Vibhore Bhatnagar <vibhore.bhatnagar@gmail.com>
2014-12-31 14:31:40 -0600

    Add Solution for exercise 'Angry Children'

---
+++ Algorithms/Warmup/Angry Children/Solution.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+// The part of the program involving reading from STDIN and writing to STDOUT has been provided by us.
+
+public class Solution {
+   static BufferedReader in = new BufferedReader(new InputStreamReader(
+         System.in));
+   static StringBuilder out = new StringBuilder();
+
+   public static void main(String[] args) throws NumberFormatException, IOException {
+      int numPackets = Integer.parseInt(in.readLine());
+      int numKids = Integer.parseInt(in.readLine());
+      int[] packets = new int[numPackets];
+      int counter=0;
+      
+      for(int i = 0; i < numPackets; i ++)
+      {
+         packets[i] = Integer.parseInt(in.readLine());
+      }
+      
+      int unfairness = Integer.MAX_VALUE;
+      
+        // Write your code here, to process numPackets N, numKids K, and the packets of candies
+      // Compute the ideal value for unfairness over here
+       Arrays.sort(packets);
+       
+       while(counter+numKids-1<numPackets)
+       {    
+           int min = packets[counter+numKids-1] - packets[counter];
+           if(min<unfairness) unfairness=min;
+           counter++;
+       }
+       
+      System.out.println(unfairness);
+   }
+}
